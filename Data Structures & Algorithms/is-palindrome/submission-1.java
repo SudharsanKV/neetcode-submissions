827998dc881f2e310f51a5cefa012acc361d7179
SudharsanKV <70580260+SudharsanKV@users.noreply.github.com>
2026-07-16 21:58:39 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int i = 0;
+        int j = s.length()-1;
+
+        while(i<j){
+            while(i<j && !isAlphaNumeric(s.charAt(i))){
+                i++;
+            }
+            while(j>i && !isAlphaNumeric(s.charAt(j))){
+                j--;
+            }
+            if(Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))){
+                return false;
+            }
+            i++;
+            j--;
+
+        }
+        return true;
+        
+    }
+    public boolean isAlphaNumeric(char c){
+        return (c>='A' && c<='Z' || c>='a' && c<='z' || c>='0' && c<='9');
+    }
+}
